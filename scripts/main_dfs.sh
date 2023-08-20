330484736342bf1bd329339168d458f7a4b970b0
tomyaacov <tomyaacov1210@gmail.com>
2023-08-20 18:26:31 +0300

    run single script

---
+++ scripts/main_dfs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sbatch scripts/run_main_single_dfs.sh --n 5 --k 1 --m 1
+sbatch scripts/run_main_single_dfs.sh --n 6 --k 1 --m 1
--- scripts/run_main_single_dfs.sh
+++ scripts/run_main_single_dfs.sh
@@ -8,11 +8,10 @@
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=32G ### total amount of RAM // 500
 #SBATCH --ntasks=1
-#SBATCH --gpus=1
 
 ### Start you code below ####
 
-out_file_name="output/"
+out_file_name="output/dfs_"
 while [ $# -gt 0 ]; do
     if [[ $1 == "--"* ]]; then
         v="${1/--/}"
@@ -27,4 +26,4 @@ done
 module load anaconda ### load anaconda module
 source activate BPpyLiveness ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPpyLiveness/ || exit
-/usr/bin/time -a -o $out_file_name -f "time:%E,memory:%M" python main_single.py --n $n --k $k --m $m --total_timesteps $total_timesteps --state_mode $state_mode --reward_mode $reward_mode > $out_file_name
+/usr/bin/time -a -o $out_file_name -f "time:%E,memory:%M" python main_dfs_single.py --n $n --k $k --m $m > $out_file_name
