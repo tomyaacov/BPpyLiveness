ecceeeeb795018470f4cc063846f25a0ad3c44f1
tomyaacov <tomyaacov1210@gmail.com>
2024-01-21 09:18:59 +0200

    updating experiment

--- scripts/main_dfs.sh
+++ scripts/main_dfs.sh
@@ -1,116 +1,116 @@
 #!/bin/bash
 
-sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 1 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 1 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 1 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 1 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 1 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 1 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 1 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 1 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 1 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 1 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 1 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 1 --m 1
 
-sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 2 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 2 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 2 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 2 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 2 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 2 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 2 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 2 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 2 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 2 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 2 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 2 --m 1
 
-sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 3 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 3 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 3 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 3 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 3 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 3 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 3 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 3 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 3 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 3 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 3 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 3 --m 1
 
-sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 4 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 4 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 4 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 4 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 4 --m 1
-sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 4 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 4 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 100 --k 4 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 150 --k 4 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 200 --k 4 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 250 --k 4 --m 1
+#sbatch scripts/run_main_single_dfs_small.sh --n 300 --k 4 --m 1
 
-sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 1 --m 2
-sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 1 --m 2
-sbatch scripts/run_main_single_dfs_medium.sh --n 150 --k 1 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 1 --m 2
+#sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 1 --m 2
+#sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 1 --m 2
+#sbatch scripts/run_main_single_dfs_medium.sh --n 150 --k 1 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 1 --m 2
 sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 1 --m 2
 sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 1 --m 2
 
-sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 2 --m 2
-sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 2 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 2 --m 2
+#sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 2 --m 2
+#sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 2 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 2 --m 2
 sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 2 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 2 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 2 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 2 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 2 --m 2
 
-sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 3 --m 2
-sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 3 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 3 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 3 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 3 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 3 --m 2
+#sbatch scripts/run_main_single_dfs_small.sh --n 50 --k 3 --m 2
+#sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 3 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 3 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 3 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 3 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 3 --m 2
 
-sbatch scripts/run_main_single_dfs_medium.sh --n 50 --k 4 --m 2
-sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 4 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 4 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 4 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 4 --m 2
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 4 --m 2
+#sbatch scripts/run_main_single_dfs_medium.sh --n 50 --k 4 --m 2
+#sbatch scripts/run_main_single_dfs_medium.sh --n 100 --k 4 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 4 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 4 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 4 --m 2
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 4 --m 2
 
-sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 1 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 1 --m 3
 sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 1 --m 3
 sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 1 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 1 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 1 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 1 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 1 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 1 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 1 --m 3
 
-sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 2 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 2 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 2 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 2 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 2 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 2 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 2 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 2 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 2 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 2 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 2 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 2 --m 3
 
-sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 3 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 3 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 3 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 3 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 3 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 3 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 3 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 3 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 3 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 3 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 3 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 3 --m 3
 
 sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 4 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 4 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 4 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 4 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 4 --m 3
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 4 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 4 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 4 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 4 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 4 --m 3
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 4 --m 3
 
-sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 1 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 1 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 1 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 1 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 1 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 1 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 1 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 1 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 1 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 1 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 1 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 1 --m 4
 
-sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 2 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 2 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 2 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 2 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 2 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 2 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 2 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 2 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 2 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 2 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 2 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 2 --m 4
 
-sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 3 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 3 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 3 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 3 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 3 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 3 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 3 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 3 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 3 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 3 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 3 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 3 --m 4
 
-sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 4 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 4 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 4 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 4 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 4 --m 4
-sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 4 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 50 --k 4 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 100 --k 4 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 150 --k 4 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 200 --k 4 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 250 --k 4 --m 4
+#sbatch scripts/run_main_single_dfs_large.sh --n 300 --k 4 --m 4
 
 
 
